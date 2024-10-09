37cb903fd6212bcb76d1a6793e5da8d0dc8dd5bc
Gerodotry <60816964+Gerodotry@users.noreply.github.com>
2024-10-09 11:41:59 +0300

    Update views.py

--- products/views.py
+++ products/views.py
@@ -2,6 +2,9 @@ from django.http import JsonResponse
 from django.urls import path
 
 def product_detail(request, productId):
+    a = "qweqew"
+    b = "324234"
+    c = "asdasdqewqwe"
     return JsonResponse({"id": str(productId), "name": f"{productId} name"})
 
 urlpatterns = [
